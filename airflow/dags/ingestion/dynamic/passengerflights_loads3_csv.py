49af9a4ce959d20edfdd49daa354f9a4e8765c7c
AnByeongHan <112845598+Kairo0628@users.noreply.github.com>
2025-11-21 11:45:24 +0900

    FEAT: 여객기 현황 데이터 로드 코드 수정  #20
    S3 -> Snowflake 과정 추가
    테이블, Dag, task명 변경
    Trigger -> 테이블 생성 태스크 추가

--- airflow/dags/ingestion/dynamic/passengerflights_loads3_csv.py
+++ airflow/dags/ingestion/dynamic/passengerflights_loads3_csv.py
@@ -1,14 +1,18 @@
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from snowflake.connector.pandas_tools import write_pandas
 from airflow.models import Variable
 from airflow.decorators import task
 from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from plugins.SlackAlert import send_slack_success_callback, send_slack_failure_callback
 
 import logging
 import requests
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
-from io import StringIO
+from io import StringIO, BytesIO
 
 def get_s3_conn():
     hook = S3Hook(aws_conn_id = 's3_conn_id')
@@ -43,6 +47,65 @@ def get_flight_data(key):
     logging.info('Get Data')
     return items
 
+def create_s3_to_snowflake_task(
+    task_id: str,
+    s3_bucket: str,
+    s3_key: str,
+    snowflake_conn_id: str,
+    database: str,
+    schema: str,
+    table_name: str,    
+):
+    @task(
+        task_id=task_id
+    )
+    def s3_to_snowflake():
+        # 1️⃣ S3 CSV 가져오기
+        s3_hook = S3Hook(aws_conn_id="s3_conn_id")
+        obj = s3_hook.get_key(s3_key, bucket_name=s3_bucket)
+        df = pd.read_csv(BytesIO(obj.get()['Body'].read()), encoding='utf-8-sig')
+
+        if df.empty:
+            raise ValueError(f"S3 파일이 비어있습니다: s3://{s3_bucket}/{s3_key}")
+
+        # 2️⃣ Snowflake Hook
+        hook = SnowflakeHook(snowflake_conn_id=snowflake_conn_id)
+        conn = hook.get_conn()  # 실제 커넥션 객체 가져오기
+        cs = conn.cursor()
+
+        try:
+            def map_dtype_to_snowflake(dtype):
+                if pd.api.types.is_integer_dtype(dtype):
+                    return "NUMBER"
+                elif pd.api.types.is_float_dtype(dtype):
+                    return "FLOAT"
+                elif pd.api.types.is_datetime64_any_dtype(dtype):
+                    return "TIMESTAMP_NTZ"
+                else:
+                    return "STRING"
+    
+            # 컬럼 생성
+            create_cols = ", ".join([f'{c} {map_dtype_to_snowflake(dtype)}' for c, dtype in df.dtypes.items()])
+            # 3️⃣ 테이블 생성
+            print(create_cols)
+            cs.execute(f"""
+                CREATE TABLE IF NOT EXISTS {database}.{schema}.{table_name} ({create_cols})
+            """)
+
+            # 4️⃣ pandas → Snowflake 적재
+            success, nchunks, nrows, _ = write_pandas(
+                conn, df, table_name, schema=schema, database=database
+            )
+            if success:
+                logging.info(f"Snowflake 적재 완료 ✅ Total rows: {nrows}")
+            else:
+                logging.error("Snowflake 적재 실패 ❌")
+        finally:
+            cs.close()
+            conn.close()
+
+    return s3_to_snowflake()
+
 @task(multiple_outputs = True)
 def extract():
     items1 = get_flight_data('arrival')
@@ -66,10 +129,10 @@ def transform(items):
 
 @task
 def transform_arrival(items):
-    columns = ['AIRLINE', 'FLIGHTID', 'SCHEDULEDATETIME', 'ESTIMATEDDATETIME',
-               'AIRPORT', 'GATENUMBER', 'CAROUSEL', 'EXITNUMBER', 'CODESHARE',
-               'MASTERFLIGHTID', 'REMARK', 'AIRPORTCODE', 'TERMINALID',
-               'TYPEOFFLIGHT', 'FID', 'FSTANDPOSITION']
+    columns = ['AIRLINE', 'FLIGHT_CD', 'SCHEDULED_TM', 'ESTIMATED_TM',
+               'CITY_KOR', 'GATE', 'CAROUSEL', 'EXIT', 'CODESHARE',
+               'MASTERID', 'STATUS', 'IATA_CD', 'TERMINAL',
+               'FLIGHT_TYPE', 'FID', 'FSTANDPOSITION']
 
     itemsToList = transform(items)
     print(items[:10])
@@ -79,22 +142,29 @@ def transform_arrival(items):
     # master 비행기의 masterflightid를 자신의 id로 변경
     # remark == NaN 제거
     # 결항, 회항이 아닌데 NaN이 있는 데이터 제거 -> 결항, 회항 NaN 유지
-    df = df.drop(['CAROUSEL', 'FID', 'FSTANDPOSITION'], axis = 1)
+    df = df.drop(['CITY_KOR', 'CAROUSEL', 'FID', 'FSTANDPOSITION'], axis = 1)
     df = df.replace(['', None], np.nan)
     cond = df['CODESHARE'] == 'Master'
-    df.loc[cond, 'MASTERFLIGHTID'] = df.loc[cond, 'FLIGHTID']
-    df = df[df['REMARK'].notna()]
-    df = df[~((df['REMARK'] != '결항') & (df.isna().any(axis = 1)))]
+    df.loc[cond, 'MASTERID'] = df.loc[cond, 'FLIGHT_CD']
+    df = df[df['STATUS'].notna()]
+    df = df[~((df['STATUS'] != '결항') & (df.isna().any(axis = 1)))]
+    # 컬럼 순서 변경
+    df = df[['AIRLINE', 'IATA_CD', 'FLIGHT_CD', 'TERMINAL',
+             'GATE', 'EXIT', 'CODESHARE', 'MASTERID',
+             'SCHEDULED_TM', 'ESTIMATED_TM', 'STATUS', 'FLIGHT_TYPE']]
+    # 시간 형식 변경
+    df['SCHEDULED_TM'] = pd.to_datetime(df['SCHEDULED_TM'], format = '%Y%m%d%H%M')
+    df['ESTIMATED_TM'] = pd.to_datetime(df['ESTIMATED_TM'], format = '%Y%m%d%H%M')    
 
     logging.info('Transform Arrival Data Complete')
     return df
 
 @task
 def transform_departure(items):
-    columns = ['AIRLINE', 'FLIGHTID', 'SCHEDULEDATETIME', 'ESTIMATEDDATETIME',
-               'AIRPORT', 'CHKINRANGE', 'GATENUMBER', 'CODESHARE',
-               'MASTERFLIGHTID', 'REMARK', 'AIRPORTCODE', 'TERMINALID',
-               'TYPEOFFLIGHT', 'FID', 'FSTANDPOSITION']
+    columns = ['AIRLINE', 'FLIGHT_CD', 'SCHEDULED_TM', 'ESTIMATED_TM',
+               'CITY_KOR', 'CHECKIN_COUNTER', 'GATE', 'CODESHARE',
+               'MASTERID', 'STATUS', 'IATA_CD', 'TERMINAL',
+               'FLIGHT_TYPE', 'FID', 'FSTANDPOSITION']
     
     itemsToList = transform(items)
     print(items[:10])
@@ -103,11 +173,18 @@ def transform_departure(items):
     # '', None -> np.nan 변경
     # master 비행기의 masterflightid를 자신의 id로 변경
     # 결항이 아닌데 NaN이 있는 데이터 제거 -> 결항 NaN 유지
-    df = df.drop(['FID', 'FSTANDPOSITION'], axis = 1)
+    df = df.drop(['CITY_KOR', 'FID', 'FSTANDPOSITION'], axis = 1)
     df = df.replace(['', None], np.nan)
     cond = df['CODESHARE'] == 'Master'
-    df.loc[cond, 'MASTERFLIGHTID'] = df.loc[cond, 'FLIGHTID']
-    df = df[~((df['REMARK'] != '결항') & (df.isna().any(axis = 1)))]
+    df.loc[cond, 'MASTERID'] = df.loc[cond, 'FLIGHT_CD']
+    df = df[~((df['STATUS'] != '결항') & (df.isna().any(axis = 1)))]
+    # 컬럼 순서 변경
+    df = df[['AIRLINE', 'IATA_CD', 'FLIGHT_CD', 'TERMINAL',
+             'GATE', 'CHECKIN_COUNTER', 'CODESHARE', 'MASTERID',
+             'SCHEDULED_TM', 'ESTIMATED_TM', 'STATUS', 'FLIGHT_TYPE']]
+    # 시간 형식 변경
+    df['SCHEDULED_TM'] = pd.to_datetime(df['SCHEDULED_TM'], format = '%Y%m%d%H%M')
+    df['ESTIMATED_TM'] = pd.to_datetime(df['ESTIMATED_TM'], format = '%Y%m%d%H%M')
 
     logging.info('Transform Departure Data Complete')
     return df
@@ -138,18 +215,50 @@ def load(df1, df2):
     logging.info('Passenger Flights Data Load Complete')
 
 with DAG(
-    dag_id = 'passenger_flights_to_s3',
+    dag_id = '3days_to_s3',
     start_date = datetime(2025, 11, 17),
     schedule = '0 5 * * *', # 14(5 + 9)시 스케줄링
     catchup = False,
     default_args = {
         'retries': 3,
-        'retry_delay' : timedelta(minutes = 3)
-    }
+        'retry_delay' : timedelta(minutes = 3),     
+    },
+    on_success_callback = send_slack_success_callback,
+    on_failure_callback = send_slack_failure_callback
 ) as dag:
     
     items = extract()
     items1, items2 = items['items1'], items['items2']
     df1 = transform_arrival(items1)
     df2 = transform_departure(items2)
-    load(df1, df2)
+    load_task = load(df1, df2)
+
+    DATABASE = 'TEAM_7_CHILL'
+    SCHEMA = 'BRONZE'
+
+    snowflake_task1 = create_s3_to_snowflake_task(
+        task_id = 'passenger_arrival_to_snowflake',
+        s3_bucket = Variable.get('bucket_name'),
+        s3_key = 'dynamic/passenger_arrivals_daily.csv',
+        snowflake_conn_id = 'snowflake_conn_id',
+        database = DATABASE,
+        schema = SCHEMA,
+        table_name = 'BR_3DAYS_ARRIVALS_DAILY'
+    )
+
+    snowflake_task2 = create_s3_to_snowflake_task(
+        task_id = 'passenger_departure_to_snowflake',
+        s3_bucket = Variable.get('bucket_name'),
+        s3_key = 'dynamic/passenger_departures_daily.csv',
+        snowflake_conn_id = 'snowflake_conn_id',
+        database = DATABASE,
+        schema = SCHEMA,
+        table_name = 'BR_3DAYS_DEPARTURE_DAILY'
+    )
+
+    trigger_next_dag = TriggerDagRunOperator(
+        task_id = 'next_dag_trigger',
+        trigger_dag_id = '3dyas_create_sl_gd_table'
+    )
+
+    load_task >> snowflake_task1 >> snowflake_task2 >> trigger_next_dag
